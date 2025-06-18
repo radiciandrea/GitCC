2b7144163fda50274057ce1fd456fcf6d417ebed
radiciandrea <andrea.radici93@libero.it>
2025-06-18 15:48:04 +0200

    corrected namings + missing "exp" in recalculating derivatives

--- 02b_MM_integration_functions.R
+++ 02b_MM_integration_functions.R
@@ -39,24 +39,24 @@ dfLog1 <- function(t, x, parms) {
       ((tasMax+tasMin)/2 - (tasMax-tasMin)/2*cos(pi*(tH-tSr[tN, ])/(14-tSr[tN, ])))*(tH>tSr[tN, ])*(tH<14)+
       ((tasMax+tasMin)/2 + (tasMax-tasMin)/2*cos(pi*(tH-14)/(10+tSr[tN, ])))*(tH>14)
     
-    delta_J = 1/(83.85 - 4.89*tempH + 0.08*tempH^2) #juvenile development rate (in SI: 82.42 - 4.87*tempH + 0.08*tempH^ 2)
-    delta_I = 1/(50.1 - 3.574*tempH + 0.069*tempH^2) #first pre blood mean rate
-    mu_E = -log(0.955 * exp(-0.5*((tempH-18.8)/21.53)^6)) # egg mortality rate
-    mu_J = -log(0.977 * exp(-0.5*((tempH-21.8)/16.6)^6)) # juvenile mortality rate
+    deltaJ = 1/(83.85 - 4.89*tempH + 0.08*tempH^2) #juvenile development rate (in SI: 82.42 - 4.87*tempH + 0.08*tempH^ 2)
+    deltaI = 1/(50.1 - 3.574*tempH + 0.069*tempH^2) #first pre blood mean rate
+    muE = -log(0.955 * exp(-0.5*((tempH-18.8)/21.53)^6)) # egg mortality rate
+    muJ = -log(0.977 * exp(-0.5*((tempH-21.8)/16.6)^6)) # juvenile mortality rate
     beta = (33.2*exp(-0.5*((tempH-70.3)/14.1)^2)*(38.8 - tempH)^1.5)*(tempH<= 38.8) #fertility rate
     
     # ODE definition 
-    dlogE1 = beta*(1-omega[t_n, ])*(exp(logA1)-1) - (h[t_n, ]*delta_E + mu_E)*(exp(logE1)-1)
-    dlogJ1 = h[t_n, ]*(delta_E*(exp(logE1)-1) + sigma[t_n, ]*gamma*(exp(logEd1)-1)) - (delta_J + mu_J + (exp(logJ1)-1)/K[t_n, ])*(exp(logJ1)-1)  
-    dlogI1 = 0.5*delta_J*(exp(logJ1)-1) - (delta_I + mu_A[t_n, ])*(exp(logI1)-1)
-    dlogA1 = delta_I*(exp(logI1)-1) - mu_A[t_n, ]*(exp(logA1)-1)
-    dlogEd1 = beta*omega[t_n, ]*(exp(logA1)-1) -  h[t_n, ]*sigma[t_n, ]*(exp(logE1)-1)
+    dlogE1 = beta*(1-omega[tN, ])*(exp(logA1)-1) - (h[tN, ]*deltaE + muE)*(exp(logE1)-1)
+    dlogJ1 = h[tN, ]*(deltaE*(exp(logE1)-1) + sigma[tN, ]*gamma*(exp(logEd1)-1)) - (deltaJ + muJ + (exp(logJ1)-1)/K[tN, ])*(exp(logJ1)-1)  
+    dlogI1 = 0.5*deltaJ*(exp(logJ1)-1) - (deltaI + muA[tN, ])*(exp(logI1)-1)
+    dlogA1 = deltaI*(exp(logI1)-1) - muA[tN, ]*(exp(logA1)-1)
+    dlogEd1 = beta*omega[tN, ]*(exp(logA1)-1) -  h[tN, ]*sigma[tN, ]*(exp(logE1)-1)
     
-    dlogE1 = dlogE1/logE1
-    dlogJ1 = dlogJ1/logJ1
-    dlogI1 = dlogI1/logI1
-    dlogA1 = dlogA1/logA1
-    dlogEd1 = dlogEd1/logEd1
+    dlogE1 = dlogE1/exp(logE1)
+    dlogJ1 = dlogJ1/exp(logJ1)
+    dlogI1 = dlogI1/exp(logI1)
+    dlogA1 = dlogA1/exp(logA1)
+    dlogEd1 = dlogEd1/exp(logEd1)
     
     dlog1x <- c(dlogE1, dlogJ1, dlogI1, dlogA1, dlogEd1)
     
