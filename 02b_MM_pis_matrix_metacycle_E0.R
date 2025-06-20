dfa3e6ec50ef9892bc7c76c82b1a6cd4e104a0be
radiciandrea <andrea.radici93@libero.it>
2025-06-20 15:41:38 +0200

    written file to launch metacycle for E0

---
+++ 02b_MM_pis_matrix_metacycle_E0.R
@@ -0,0 +1,38 @@
+# metacycle to run cycle
+
+IDsSubSet = 1:8981 # put to compute only a subset of cells (8981 in total)
+
+## HIST----
+
+name = "Hist"
+years = 1996:2005 #:2005
+
+source("02_MM_pis_matrix_cycle_E0.R")
+
+## SSP2 RCP 4.5 2055----
+
+name = "ssp245"
+years = 2050:2059
+
+source("02_MM_pis_matrix_cycle_E0.R")
+
+## SSP2 RCP 4.5 2085----
+
+name = "ssp245"
+years = 2080:2089
+
+source("02_MM_pis_matrix_cycle_E0.R")
+
+## SSP5 RCP 8.5 2055----
+
+name = "ssp585"
+years = 2050:2059
+
+source("02_MM_pis_matrix_cycle_E0.R")
+
+## SSP5 RCP 8.5 2085----
+
+name = "ssp585"
+years = 2080:2089
+
+source("02_MM_pis_matrix_cycle_E0.R")
