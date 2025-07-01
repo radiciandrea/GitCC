d0672b799100ff9d627db0f710d21f42e2d71e06
radiciandrea <andrea.radici93@libero.it>
2025-07-01 12:14:22 +0200

    created metacycle code

---
+++ 03b_MM_pis_matrix_metacycle_Abundances.R
@@ -0,0 +1,42 @@
+# metacycle to run cycle to simulate abundances
+
+rm(list = ls())
+
+IDsSubSet = 1:8981 # put to compute only a subset of cells (8981 in total)
+
+nRep = 5
+
+## HIST----
+
+name = "Hist"
+years = 1996:2005
+
+source("03b_MM_pis_matrix_cycle_Abundances.R")
+
+## SSP2 RCP 4.5 2055----
+
+name = "ssp245"
+years = 2050:2059
+
+source("03b_MM_pis_matrix_cycle_Abundances.R")
+
+## SSP2 RCP 4.5 2085----
+
+name = "ssp245"
+years = 2080:2089
+
+source("03b_MM_pis_matrix_cycle_Abundances.R")
+
+## SSP5 RCP 8.5 2055----
+
+name = "ssp585"
+years = 2050:2059
+
+source("03b_MM_pis_matrix_cycle_Abundances.R")
+
+## SSP5 RCP 8.5 2085----
+
+name = "ssp585"
+years = 2080:2089
+
+source("03b_MM_pis_matrix_cycle_Abundances.R")
