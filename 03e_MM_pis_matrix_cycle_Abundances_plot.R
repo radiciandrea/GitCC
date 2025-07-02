895acd6fae789d800734ca0cc119552cdf76d56a
radiciandrea <andrea.radici93@libero.it>
2025-07-02 10:30:29 +0200

    added section "Introduction"
    added scenario df at the beginning
    
    corrected colSums in colMean
    
    added plot for Adults and RO

--- 03e_MM_pis_matrix_cycle_Abundances_plot.R
+++ 03e_MM_pis_matrix_cycle_Abundances_plot.R
@@ -15,7 +15,7 @@ folderDrias = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/D
 folderPlotA = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Scenari climatici/Adults"
 folderPlotR0 = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Scenari climatici/R0"
 
-# Common initial settings
+## initial settings----
 
 # load 1 for dimension
 files = list.files(paste0(folderData,"/"), paste0("Sim_Drias_", name))
@@ -24,6 +24,13 @@ nC = 5 # number of classes
 nIDs = (ncol(Sim)-1)/nC # number of regions
 IDs = 1:nIDs
 IDsSubSet = IDs
+
+# scenarios
+
+scenariosDF= data.frame(name = c("Hist", "ssp245", "ssp245", "ssp585", "ssp585"),
+                          yearStart = c(1996, 2050, 2080, 2050, 2080),
+                          yearEnd = c(1996, 2050, 2080, 2050, 2080)+10)
+
 
 # create meta matrices for each scenario (hist, ssp2, ssp5) for both adults and MTS for dengue
 
@@ -110,8 +117,8 @@ for(i in  1:length(years)){
 
 rm(IDsDT)
 
-AmjjasoMM[1,] <- colSums(AmjjasoM, na.rm =T)
-LTSdengueMM[1,] <- colSums(LTSdengueM, na.rm =T)
+AmjjasoMM[1,] <- colMeans(AmjjasoM, na.rm =T)
+LTSdengueMM[1,] <- colMeans(LTSdengueM, na.rm =T)
 
 ## SSP2 RCP 4.5 2055----
 
@@ -180,8 +187,8 @@ for(i in  1:length(years)){
 
 rm(IDsDT)
 
-AmjjasoMM[2,] <- colSums(AmjjasoM, na.rm =T)
-LTSdengueMM[2,] <- colSums(LTSdengueM, na.rm =T)
+AmjjasoMM[2,] <- colMeans(AmjjasoM, na.rm =T)
+LTSdengueMM[2,] <- colMeans(LTSdengueM, na.rm =T)
 
 ## SSP2 RCP 4.5 2085----
 
@@ -250,8 +257,8 @@ for(i in  1:length(years)){
 
 rm(IDsDT)
 
-AmjjasoMM[3,] <- colSums(AmjjasoM, na.rm =T)
-LTSdengueMM[3,] <- colSums(LTSdengueM, na.rm =T)
+AmjjasoMM[3,] <- colMeans(AmjjasoM, na.rm =T)
+LTSdengueMM[3,] <- colMeans(LTSdengueM, na.rm =T)
 
 
 ## SSP5 RCP 8.5 2055----
@@ -321,8 +328,8 @@ for(i in  1:length(years)){
 
 rm(IDsDT)
 
-AmjjasoMM[4,] <- colSums(AmjjasoM, na.rm =T)
-LTSdengueMM[4,] <- colSums(LTSdengueM, na.rm =T)
+AmjjasoMM[4,] <- colMeans(AmjjasoM, na.rm =T)
+LTSdengueMM[4,] <- colMeans(LTSdengueM, na.rm =T)
 
 ## SSP5 RCP 8.5 2085----
 
@@ -391,15 +398,76 @@ for(i in  1:length(years)){
 
 rm(IDsDT)
 
-AmjjasoMM[5,] <- colSums(AmjjasoM, na.rm =T)
-LTSdengueMM[5,] <- colSums(LTSdengueM, na.rm =T)
+AmjjasoMM[5,] <- colMeans(AmjjasoM, na.rm =T)
+LTSdengueMM[5,] <- colMeans(LTSdengueM, na.rm =T)
 
-## Save and load----
-saveRDS(AmjjasoMM, file = paste0(folderData, "/AmjjasoMM.rds", ))
-saveRDS(LTSdengueMM, file = paste0(folderData, "/LTSdengueMM.rds", ))
+# Save and load----
+saveRDS(AmjjasoMM, file = paste0(folderData, "/AmjjasoMM.rds"))
+saveRDS(LTSdengueMM, file = paste0(folderData, "/LTSdengueMM.rds"))
 
-AmjjasoMM <- readRDS(file = paste0(folderData, "/AmjjasoMM.rds", ))
-LTSdengueMM <- readRDS(file = paste0(folderData, "/LTSdengueMM.rds", ))
+AmjjasoMM <- readRDS(file = paste0(folderData, "/AmjjasoMM.rds"))
+LTSdengueMM <- readRDS(file = paste0(folderData, "/LTSdengueMM.rds"))
 
-### plot
+# Plot----
+
+# Load map
+domain <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/SafranDomain.shp")
+
+## Adults ----
+
+cutPal = c(10^3, 10^2, 10^1, 1, 1)
+cutPalLab = c("e > 1000", "d > 100", "c > 10", "b > 1", "a < 1")
+colPal<- c( "#a63603", "#a63603", "#e6550d", "#fd8d3c", "#fdbe85", "#feedde")
+
+# Cycle
+
+for(i in 1:nrow(scenariosDF)){
+  name = scenariosDF$name[i]
+  years = scenariosDF$yearStart[i]:scenariosDF$yearEnd[i]
+  
+  AmjjasoCut <- case_when(AmjjasoMM[i,] >= cutPal[1] ~ cutPalLab[1],
+                         AmjjasoMM[i,] >= cutPal[2] ~ cutPalLab[2],
+                         AmjjasoMM[i,] >= cutPal[3] ~ cutPalLab[3],
+                         AmjjasoMM[i,] >= cutPal[4] ~ cutPalLab[4],
+                         AmjjasoMM[i,] <= cutPal[4] ~ cutPalLab[5])
+  
+  plotCut <- ggplot()+
+    geom_sf(data = domain, aes(fill = AmjjasoCut), colour = NA)+ #
+    scale_fill_manual(values = colPal)+
+    ggtitle(paste0("LTS (dengue), scenario: ", name, "; period: ", min(years), "-", max(years)))
+  
+  ggsave(file = 
+           paste0(folderPlotA, "/Amjjaso_", name, "_", min(years), "-", max(years), ".png"),
+         plot= plotCut , units="in", width=5.5, height=7, dpi=300)
+
+}
+
+## R0 ----
+
+cutPal = c(105, 56, 21, 1, 0)
+cutPalLab = c("e 15 or more", "d 8 to 15", "c 3 to 8", "b 0 to 3", "a 0")
+colPal<- c("#450054", "#3A528A", "#21908C", "#5CC963", "#FCE724")
+
+# Cycle
+
+for(i in 1:nrow(scenariosDF)){
+  name = scenariosDF$name[i]
+  years = scenariosDF$yearStart[i]:scenariosDF$yearEnd[i]
+  
+  LTSSelCut <- case_when(LTSdengueMM[i,] >= cutPal[1] ~ cutPalLab[1],
+                         LTSdengueMM[i,] >= cutPal[2] ~ cutPalLab[2],
+                         LTSdengueMM[i,] >= cutPal[3] ~ cutPalLab[3],
+                         LTSdengueMM[i,] >= cutPal[4] ~ cutPalLab[4],
+                         LTSdengueMM[i,] <= cutPal[4] ~ cutPalLab[5])
+  
+  plotCut <- ggplot()+
+    geom_sf(data = domain, aes(fill = LTSSelCut), colour = NA)+ #
+    scale_fill_manual(values = colPal)+
+    ggtitle(paste0("LTS (dengue), scenario: ", name, "; period: ", min(years), "-", max(years)))
+  
+  ggsave(file = 
+           paste0(folderPlotR0, "/LTS_dengue_", name, "_", min(years), "-", max(years), ".png"),
+         plot= plotCut , units="in", width=5.5, height=7, dpi=300)
+  
+}
 
