e2fcbb11fbd2cdf0b39b0199679dfad679ac4011
radiciandrea <andrea.radici93@libero.it>
2025-07-08 17:18:21 +0200

    correccted name cycle to cycle

--- 04c_MM_SEI_pis_matrix_metacycle_SecondayCases.R
+++ 04c_MM_SEI_pis_matrix_metacycle_SecondayCases.R
@@ -9,32 +9,32 @@ IDsSubSet = 1:8981 # put to compute only a subset of cells (8981 in total)
 name = "Hist"
 years = 1996:2005 #:2005
 
-source("02_MM_pis_matrix_cycle_E0.R")
+source("04a_MM_SEI_pis_matrix_cycle_SecondayCases.R")
 
 ## SSP2 RCP 4.5 2055----
 
 name = "ssp245"
 years = 2050:2059
 
-source("02_MM_pis_matrix_cycle_E0.R")
+source("04a_MM_SEI_pis_matrix_cycle_SecondayCases.R")
 
 ## SSP2 RCP 4.5 2085----
 
 name = "ssp245"
 years = 2080:2089
 
-source("02_MM_pis_matrix_cycle_E0.R")
+source("04a_MM_SEI_pis_matrix_cycle_SecondayCases.R")
 
 ## SSP5 RCP 8.5 2055----
 
 name = "ssp585"
 years = 2050:2059
 
-source("02_MM_pis_matrix_cycle_E0.R")
+source("04a_MM_SEI_pis_matrix_cycle_SecondayCases.R")
 
 ## SSP5 RCP 8.5 2085----
 
 name = "ssp585"
 years = 2080:2089
 
-source("02_MM_pis_matrix_cycle_E0.R")
+source("04a_MM_SEI_pis_matrix_cycle_SecondayCases.R")
