2bb1a6bcfe10bbf1d060296ed034e9e22d5dd0be
radiciandrea <andrea.radici93@libero.it>
2025-08-12 18:11:23 +0200

    created file to process data insee

---
+++ 01_spatial_data_preprocessing_Omphale.R
@@ -0,0 +1,18 @@
+#### spatial data pre processing ----
+# using OMPHALE 2022
+
+# data from https://www.insee.fr/fr/statistiques/6652134?sommaire=6652140
+ 
+# explication from http://www.progedo-adisp.fr/apf_projpop.php#:~:text=Qu'est%2Dce%20que%20l,de%20sc%C3%A9narios%20pour%20le%20futur
+
+
+# reminder of ssp scenarios:
+# ssp 5 - "fossil fuelled development". Maybe associated with "hypothèse haute", even if population in ssp5 is supposed to decline... 
+# ssp 2 - "median hypothesis" -> associated with central scenario
+
+# new horizons: 
+# 1996-2005 (« hist, 2000 »)
+# 2035-2044 « 2040 »
+# 2065-2074 « 2070 »
+
+folderInsee <- "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/INSEE_Omphale_2022"
