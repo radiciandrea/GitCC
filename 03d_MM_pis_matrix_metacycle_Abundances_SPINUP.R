7357bb772b80388c5af2ba16547603263e4ba04e
radiciandrea <andrea.radici93@libero.it>
2025-09-09 09:56:45 +0200

    corrected year beginning

--- 03d_MM_pis_matrix_metacycle_Abundances_SPINUP.R
+++ 03d_MM_pis_matrix_metacycle_Abundances_SPINUP.R
@@ -9,34 +9,34 @@ nRep = 5
 ## historical ----
 
 name = "Hs99"
-years = 1986:2005 
+years = rep(1986, nRep) 
 
 source("03c_MM_pis_matrix_cycle_Abundances_SPINUP.R")
 
 ## Central RCP 4.5 short term----
 
 name = "Cn55"
-years = 2046:2065
+years = rep(2046, nRep) 
 
 source("03c_MM_pis_matrix_cycle_Abundances_SPINUP.R")
 
 ## Central RCP 4.5 long term----
 
 name = "Cn70"
-years = 2066:2085
+years = rep(2066, nRep)
 
 source("03c_MM_pis_matrix_cycle_Abundances_SPINUP.R")
 
 ## High RCP 8.5 short term----
 
 name = "Hg55"
-years = 2046:2065
+years = rep(2046, nRep)
 
 source("03c_MM_pis_matrix_cycle_Abundances_SPINUP.R")
 
 ## High RCP 8.5 long term----
 
 name = "Hg70"
-years = 2066:2085
+years = rep(2066, nRep)
 
 source("03c_MM_pis_matrix_cycle_Abundances_SPINUP.R")
