38d8f91070b2091c7b480975cf20d1cd2b3a0f13
radiciandrea <andrea.radici93@libero.it>
2025-10-10 14:25:27 +0200

    parallelized

--- 07c_MM_SEI_metacycle_SecondaryCases.R
+++ 07c_MM_SEI_metacycle_SecondaryCases.R
@@ -1,18 +1,28 @@
 # code to run the metacycle of the 07bSEI (running on météo france)
 
-years <- 2019:2024
+# years <- 2019:2024
+
+library(foreach)
+
+years <- 2024
 
 df_cities = data.frame(name = c("LA CRAU", "SAINTE CECILE LES VIGNES"),
                        dep = c("83", "84"),
                        weather_station = c("HYERES","ORANGE"),
                        cell = c(582, 1858),
-                       IntroCalendar  = c("05-08", "19-07")) # month, day
+                       IntroCalendar  = c("08-05", "07-19"),
+                       OutroCalendar  = c("09-05", "08-25"),
+                       X0_E0 = c(28800, 21060)) # month, day
 
-for(i in 1:nrows(df_cities)){
+foreach(i %in% 1:nrow(df_cities)){
   
   name = df_cities$name[i]
   IDsSubSet = df_cities$cell[i]
   IntroCalendar = df_cities$IntroCalendar[i]
   
   source("07b_MM_SEI_SecondaryCases.R")
+  plot((max(SH) - SH)*IDsDT$surfHa, main = name)
+  
+  saveRDS(SH, file = paste0(folderOut, "/07c_SH_", name, "_", year, ".rds"))
+  
 }
