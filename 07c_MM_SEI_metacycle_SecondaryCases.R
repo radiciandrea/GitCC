19d60cb5a55159c8119617eb89fbb4287cfb6c55
radiciandrea <andrea.radici93@libero.it>
2025-10-10 13:41:52 +0200

    description added

--- 07c_MM_SEI_metacycle_SecondaryCases.R
+++ 07c_MM_SEI_metacycle_SecondaryCases.R
@@ -1,4 +1,4 @@
-
+# code to run the metacycle of the 07bSEI (running on météo france)
 
 years <- 2019:2024
 
@@ -6,9 +6,10 @@ df_cities = data.frame(name = c("LA CRAU", "SAINTE CECILE LES VIGNES"),
                        dep = c("83", "84"),
                        weather_station = c("HYERES","ORANGE"),
                        cell = c(582, 1858),
-                       IntroCalendar  = c("05-08", "19-07")) #to be modified
+                       IntroCalendar  = c("05-08", "19-07")) # month, day
 
 for(i in 1:nrows(df_cities)){
+  
   name = df_cities$name[i]
   IDsSubSet = df_cities$cell[i]
   IntroCalendar = df_cities$IntroCalendar[i]
