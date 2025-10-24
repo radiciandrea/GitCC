5dc223695603be5b0860a148dbe79d47251957c8
radiciandrea <andrea.radici93@libero.it>
2025-10-24 16:14:30 +0200

    tranformed into a cycle

--- 07c_MM_SEI_metacycle_SecondaryCases.R
+++ 07c_MM_SEI_metacycle_SecondaryCases.R
@@ -4,8 +4,8 @@ library(foreach)
 
 rm(list = ls())
 
-years <- 2024
-# years <- 2019:2024
+# years <- 2024
+years <- 2019:2024
 
 # https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-a-transmission-vectorielle/chikungunya/articles/donnees-en-france-metropolitaine/chikungunya-dengue-et-zika-donnees-de-la-surveillance-renforcee-en-france-hexagonale-2024
 
@@ -21,20 +21,43 @@ df_cities = data.frame(name = c("LA CRAU", "SAINTE CECILE LES VIGNES", "FREJUS",
                        popKm2 = c(513, 133, 572, 2163),
                        surfHa = c(3787, 1982, 10227, 1304))
 
-# foreach(i = 1:nrow(df_cities)) %do% { # dopar
-for(i in 1:nrow(df_cities)) { # dopar
-  name = df_cities$name[i]
-  IDsSubSet = df_cities$cell[i]
-  IntroCalendar = df_cities$IntroCalendar[i]
-  X0_E0 = df_cities$X0_E0[i]
+# exp_H= 0.9 # to saturate, values between 
+
+df_Sim = data.frame(exp_H = seq(0.85, 1, length.out = 200),
+                       simLaCrau = NA,
+                       simSCeclie = NA,
+                       simFrejus = NA,
+                       simVallauris = NA)
+
+for(j in 1:nrow(df_Sim)){
+  # foreach(i = 1:nrow(df_cities)) %do% { # dopar
+  exp_H = df_Sim$exp_H[j]
   
-  source("07b_MM_SEI_SecondaryCases.R")
-  plot((max(SH) - SH)*IDsDT$surfHa, main = name)
-  
-  df_cities$simCases[i] = (max(SH) - min(SH))*IDsDT$surfHa
-  
-  rm(IDsDT)
+  for(i in 1:nrow(df_cities)) { # dopar
+    name = df_cities$name[i]
+    IDsSubSet = df_cities$cell[i]
+    IntroCalendar = df_cities$IntroCalendar[i]
+    
+    # let's consider: exp_H
+    X0_E0 = (df_cities$X0_E0[i])^exp_H
+    
+    source("07b_MM_SEI_SecondaryCases.R")
+    plot((max(SH) - SH)*IDsDT$surfHa, main = name)
+    lines(rep(df_cities$cases[i], times = length(SH)), col = 'blue')
+    
+    df_cities$simCases[i] = (max(SH) - min(SH))*IDsDT$surfHa
+    
+    df_Sim[j, i+1] = df_cities$simCases[i]
+    
+    rm(IDsDT)
+  }
 }
 
+saveRDS(df_Sim, file = "df_Sim.rds")
 
-(df_cities$simCases - df_cities$cases)/df_cities$cases
+
+# mean(sqrt((df_cities$simCases - df_cities$cases)^2))
+
+#exp_H == 1, err = 17.66691
+#exp_H == 0.95, 
+#exp_H == 0.9, err = df_cities
