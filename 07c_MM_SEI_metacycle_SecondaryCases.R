c03e9f7199a4c872b70d9a35a2dae1a1f7d8a1d9
radiciandrea <andrea.radici93@libero.it>
2025-10-24 13:53:30 +0200

    written as for (temporary)

--- 07c_MM_SEI_metacycle_SecondaryCases.R
+++ 07c_MM_SEI_metacycle_SecondaryCases.R
@@ -21,18 +21,19 @@ df_cities = data.frame(name = c("LA CRAU", "SAINTE CECILE LES VIGNES", "FREJUS",
                        popKm2 = c(513, 133, 572, 2163),
                        surfHa = c(3787, 1982, 10227, 1304))
 
-foreach(i = 1:nrow(df_cities)) %do% { # dopar
-  
+# foreach(i = 1:nrow(df_cities)) %do% { # dopar
+for(i in 1:nrow(df_cities)) { # dopar
   name = df_cities$name[i]
   IDsSubSet = df_cities$cell[i]
   IntroCalendar = df_cities$IntroCalendar[i]
-  # X0_E0 = df_cities$X0_E0[i]
+  X0_E0 = df_cities$X0_E0[i]
   
   source("07b_MM_SEI_SecondaryCases.R")
   plot((max(SH) - SH)*IDsDT$surfHa, main = name)
   
   df_cities$simCases[i] = (max(SH) - min(SH))*IDsDT$surfHa
   
+  rm(IDsDT)
 }
 
 
