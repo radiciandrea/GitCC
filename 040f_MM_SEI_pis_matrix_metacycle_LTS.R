d80b1a055cf4baaa13f39fcc41989541e3f50475
radiciandrea <andrea.radici93@libero.it>
2025-10-28 18:00:10 +0100

    created files to run LTS estimation via simulations (with modified K)

---
+++ 040f_MM_SEI_pis_matrix_metacycle_LTS.R
@@ -0,0 +1,56 @@
+# metacycle to run cycle
+# here to estiamte LTS
+
+rm(list = ls())
+
+IDsSubSet = 1:8981 # put to compute only a subset of cells (8981 in total)
+
+## historical ----
+
+name = "Hs99"
+years = 1986:2005 
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
+
+## Central RCP 4.5 short term----
+
+name = "Cn35"
+years = 2026:2045
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
+
+## Central RCP 4.5 middle term----
+
+name = "Cn55"
+years = 2046:2065
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
+
+
+## Central RCP 4.5 long term----
+
+name = "Cn70"
+years = 2066:2085
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
+
+## High RCP 8.5 short term----
+
+name = "Hg35"
+years = 2026:2045
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
+
+## High RCP 8.5 middle term----
+
+name = "Hg55"
+years = 2046:2065
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
+
+## High RCP 8.5 long term----
+
+name = "Hg70"
+years = 2066:2085
+
+source("040e_MM_SEI_pis_matrix_cycle_LTS.R")
