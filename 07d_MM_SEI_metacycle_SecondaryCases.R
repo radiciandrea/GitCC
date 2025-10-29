357d7d89ba72e25ea22173f11eccb2a16b07f40d
radiciandrea <andrea.radici93@libero.it>
2025-10-29 16:12:26 +0100

    Added settingd for save cases and load X0

--- 07d_MM_SEI_metacycle_SecondaryCases.R
+++ 07d_MM_SEI_metacycle_SecondaryCases.R
@@ -23,25 +23,26 @@ if(!exists("folderOut")){
 # years <- 2024
 years <- 2019:2025
 
-# https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-a-transmission-vectorielle/chikungunya/articles/donnees-en-france-metropolitaine/chikungunya-dengue-et-zika-donnees-de-la-surveillance-renforcee-en-france-hexagonale-2024
-
 dfCities =  data.frame(name = c("ROGNAC", "AUBAGNE"),
                        dep = c("13", "13"),
                        weather_station = c("MARIGNANE", "AUBAGNE"),
                        IntroCalendar  = c("07-05", "08-23"),
                        OutroCalendar  = c("07-27", "09-14"),
-                       X0_E0 = NA,
-                       cases = c(5, 9), 
+                       cases = c(5, 9),
+                       simCases1 = NA, #to record cases
+                       simCases091 = NA,#to record cases 
                        cell = c("882", "721"),
                        popKm2 = c(706, 869),
                        surfHa = c(1746, 5490))
 
+X0 = readRDS(file = paste0(folderX0, "/X00_Drias_Cn35_2026.rds"))
+X0[4*8981+as.numeric(dfCities$cell)]
+
+dfCities$X0091 = X0[4*8981+as.numeric(dfCities$cell)]
+
 #re-simulate
 
 expH = 0.91
-
-plot(mCases[1,], dfSim[(dfSim$exp_H == minExpHRMSLE),1 +1:4], xlim = c(0,30), ylim = c(0,30))
-lines(0:30, 0:30)
 
 for(i in 1:nrow(dfCities)) { # dopar
   name = dfCities$name[i]
@@ -49,15 +50,22 @@ for(i in 1:nrow(dfCities)) { # dopar
   IntroCalendar = dfCities$IntroCalendar[i]
   
   # let's consider: exp_H
-  X0_E0 = (dfCities$X0_E0[i])^expH
+  X0_E0 = (dfCities$X0091[i])
   
   source("07b_MM_SEI_SecondaryCases.R")
-  plot((max(SH) - SH)*IDsDT$surfHa, main = name)
+  plot((max(SH) - SH)*IDsDT$surfHa, main = paste0(name, "expH = ", expH))
   lines(rep(dfCities$cases[i], times = length(SH)), col = 'blue')
+  
+  dfCities$simCases1[i] = (max(SH) - min(SH))*IDsDT$surfHa
 }
 
 
 #re-simulate
+
+X0 = readRDS(file = paste0(folderX0, "/X0_Drias_Cn35_2026.rds"))
+X0[4*8981+as.numeric(dfCities$cell)]
+
+dfCities$X01 = X0[4*8981+as.numeric(dfCities$cell)]
 
 expH = 1
 
@@ -67,9 +75,11 @@ for(i in 1:nrow(dfCities)) { # dopar
   IntroCalendar = dfCities$IntroCalendar[i]
   
   # let's consider: exp_H
-  X0_E0 = (dfCities$X0_E0[i])^expH
+  X0_E0 = (dfCities$dfCities$X01[i])^expH
   
   source("07b_MM_SEI_SecondaryCases.R")
-  plot((max(SH) - SH)*IDsDT$surfHa, main = name)
+  plot((max(SH) - SH)*IDsDT$surfHa, main = paste0(name, "expH = ", expH))
   lines(rep(dfCities$cases[i], times = length(SH)), col = 'blue')
+  
+  dfCities$simCases1[i] = (max(SH) - min(SH))*IDsDT$surfHa
 }
