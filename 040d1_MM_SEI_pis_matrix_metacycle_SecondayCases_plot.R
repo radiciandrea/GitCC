af898fd9e7527093c3f81df16e14fbe77ed0ac68
radiciandrea <andrea.radici93@libero.it>
2025-12-08 17:32:10 +0100

    corrected how to plot in columns

--- 040d1_MM_SEI_pis_matrix_metacycle_SecondayCases_plot.R
+++ 040d1_MM_SEI_pis_matrix_metacycle_SecondayCases_plot.R
@@ -4,6 +4,8 @@
 # and MM_pis_matrix_EOBS_cycle_consec_01_plot.R
 
 library(ggplot2)
+library(ggpubr)
+library(patchwork)
 library(reshape2) 
 library(dplyr)
 library(pracma)
@@ -54,15 +56,6 @@ SecCaseOctMM <- matrix(NA, ncol = nIDs, nrow = nrow(scenariosDF)) # Secondary Ca
 SecCaseNovMM <- matrix(NA, ncol = nIDs, nrow = nrow(scenariosDF)) # Secondary Cases metamatrix
 SecCaseDecMM <- matrix(NA, ncol = nIDs, nrow = nrow(scenariosDF)) # Secondary Cases metamatrix
 
-#host preference
-phiAU = 0.9 #human biting preference (urban)
-phiAR = 0.5 #human biting preference (rural) #Caminade 2016
-RTh = 50 #defines density over km2 below which an area is "rural", with little phi
-
-IIPdengue = 5 #Benkimoun 2021
-
-bV2H = 0.5 # b Blagrove 2020
-bH2Vdengue = 0.31 # beta Metelmann 2021
 
 ## cycle to calculate indicators ----
 
@@ -73,7 +66,6 @@ for(k in 1:nrow(scenariosDF)){
   filesSH <- list.files(paste0(folderSim,"/"), paste0("040a_SH_Drias_SEIS_", name))
   
   # matrices of indicators
-  SecCaseM = matrix(NA, nrow = length(years), ncol = nIDs) # Secondary Cases matrix
   
   SecCaseJanM <- matrix(NA, nrow = length(years), ncol = nIDs) # Secondary Cases matrix
   SecCaseFebM <- matrix(NA, nrow = length(years), ncol = nIDs) # Secondary Cases matrix
@@ -88,45 +80,48 @@ for(k in 1:nrow(scenariosDF)){
   SecCaseNovM <- matrix(NA, nrow = length(years), ncol = nIDs) # Secondary Cases matrix
   SecCaseDecM <- matrix(NA, nrow = length(years), ncol = nIDs) # Secondary Cases matrix
   
+  
+  WTotDT <- readRDS(paste0(folderDrias, "/Drias_", name, "_", years[1], ".rds")) %>%
+    filter(ID %in% IDsSubSet) 
+  
+  IDsDT <- WTotDT %>%
+    distinct(ID, .keep_all = TRUE) %>%
+    dplyr::select(c("ID", "pop", "surfHa")) %>%
+    filter(ID %in% IDsSubSet)
+  
+  #area as (km²)
+  AreaKm2 = IDsDT$surfHa*10^-2
+  
   for(i in  1:length(years)){
     
     fileSH = filesSH[i]
     
     year <- years[i] # substr(file, nchar(file)-7, nchar(file)-4)
     
-    WTotDT <- readRDS(paste0(folderDrias, "/Drias_", name, "_", year, ".rds")) %>%
-      filter(ID %in% IDsSubSet) 
-    
-    IDsDT <- WTotDT %>%
-      distinct(ID, .keep_all = TRUE) %>%
-      dplyr::select(c("ID", "pop", "surfHa")) %>%
-      filter(ID %in% IDsSubSet)
-    
-    #reshape human matrix
-    H = matrix(rep(IDsDT$pop, nD), nrow = nD, byrow = T )
-    
-    #area as (km²)
-    AreaKm2 = IDsDT$surfHa*10^-2
-    
     # Secondary Cases one introduction per month---- 
     # let's take "the worst"
     
     SH <- readRDS(paste0(folderSim, "/", fileSH))
     
+    nD = nrow(SH)
+    
+    #reshape human matrix
+    H = matrix(rep(IDsDT$pop, nD), nrow = nD, byrow = T )
+    
     MaxSH <-sapply(1:ncol(SH), function(x){max(SH[,x])})
     
-    MinSHJan <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHFeb <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHMar <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHApr <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHMay <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHJun <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHJul <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHAug <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHSep <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHOct <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHNov <-sapply(1:ncol(SH), function(x){min(SH[,x])})
-    MinSHDec <-sapply(1:ncol(SH), function(x){min(SH[,x])})
+    MinSHJan <-sapply(1:ncol(SH), function(x){min(SH[1:31,x])})
+    MinSHFeb <-sapply(1:ncol(SH), function(x){min(SH[32:(nD-306),x])})
+    MinSHMar <-sapply(1:ncol(SH), function(x){min(SH[(nD-31+1):nD-275,x])})
+    MinSHApr <-sapply(1:ncol(SH), function(x){min(SH[(nD-30+1):nD-245,x])})
+    MinSHMay <-sapply(1:ncol(SH), function(x){min(SH[(nD-31+1):nD-214,x])})
+    MinSHJun <-sapply(1:ncol(SH), function(x){min(SH[(nD-30+1):nD-184,x])})
+    MinSHJul <-sapply(1:ncol(SH), function(x){min(SH[(nD-31+1):nD-153,x])})
+    MinSHAug <-sapply(1:ncol(SH), function(x){min(SH[(nD-31+1):nD-122,x])})
+    MinSHSep <-sapply(1:ncol(SH), function(x){min(SH[(nD-30+1):nD-92,x])})
+    MinSHOct <-sapply(1:ncol(SH), function(x){min(SH[(nD-31+1):nD-61,x])})
+    MinSHNov <-sapply(1:ncol(SH), function(x){min(SH[(nD-30+1):nD-31,x])})
+    MinSHDec <-sapply(1:ncol(SH), function(x){min(SH[(nD-31+1):nD,x])})
     
     MaxSecondayCasesJan = 100*(MaxSH - MinSHJan)*AreaKm2 # 100 per ha
     MaxSecondayCasesFeb = 100*(MaxSH - MinSHFeb)*AreaKm2 # 100 per ha
@@ -195,6 +190,13 @@ SecCaseList <- readRDS(file = paste0(folderSim, "/SecCaseList.rds"))
 
 # Load map
 domain <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/SafranDomain.shp")
+# domain$Hs99 = NA
+# domain$Cn35 = NA
+# domain$Cn55 = NA
+# domain$Cn70 = NA
+# domain$Hg35 = NA
+# domain$Hg55 = NA
+# domain$Hg70 = NA
 
 ### Secondary cases ----
 
@@ -210,20 +212,28 @@ for(j in 1:length(SecCaseList)){
   
   mont_j = mont_v[j]
   
+  plotList <- vector(mode = "list", length = nrow(scenariosDF))
+  
   for(i in 1:nrow(scenariosDF)){
+  
     name = scenariosDF$name[i]
     years = scenariosDF$yearStart[i]:scenariosDF$yearEnd[i]
     
-    SecCaseCut <- case_when(SecCaseMM[i,] >= cutPal[1] ~ cutPalLab[1],
+    SecCaseCut = case_when(SecCaseMM[i,] >= cutPal[1] ~ cutPalLab[1],
                             SecCaseMM[i,] >= cutPal[2] ~ cutPalLab[2],
                             SecCaseMM[i,] >= cutPal[3] ~ cutPalLab[3],
                             SecCaseMM[i,] >= cutPal[4] ~ cutPalLab[4],
                             SecCaseMM[i,] <= cutPal[4] ~ cutPalLab[5])
     
+    ind = round(100*sum(SecCaseMM[i,]>1, na.rm = T)/nIDs, 1)
+    
+    domain_i <- domain                # make a local copy
+    domain_i$SecCaseCut <- SecCaseCut # add the correct variable for this iteration
+    
     plotCut <- ggplot()+
-      geom_sf(data = domain, aes(fill = SecCaseCut), colour = NA)+ #
+      geom_sf(data = domain_i, aes(fill = SecCaseCut), colour = NA)+ #
       scale_fill_manual(values = colPal)+
-      ggtitle(paste0("Secondary cases, scenario: ", name, "; period: ", min(years), "-", max(years)))+
+      ggtitle(paste0("Sc: ", name, ",", min(years), "-", max(years)))+
       theme(plot.background  = element_blank(),
             aspect.ratio = 1)
     
@@ -234,15 +244,34 @@ for(j in 1:length(SecCaseList)){
             line = element_blank(), 
             rect = element_blank(), 
             text = element_blank(), 
-            plot.background = element_rect(fill = "transparent", color = "transparent"))
+            axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks = element_blank(),
+            plot.background = element_rect(fill = "transparent", color = "transparent"))+
+      annotate(geom="text", x=0, y=42, label=  paste0(name, ";", ind, "%"),
+               color="black")
     # }
     
-    ggsave(file = 
-             paste0(folderPlot, "/SecCase_", name, "_", min(years), "-", max(years), "_", mont_j, ".png"),
-           plot= plotCut, units="in", height=3.2, width = 4.2, dpi=300) #units="in", height=4,
-    
-    cat("name:", name, ", month:", mont_j, "SC>1: ", round(100*sum(SecCaseMM[i,]>1, na.rm = T)/8981, 0), "\n")
+    plotList[[i]] <-  plotCut
+     
+    cat("name:", name, ", month:", mont_j, "SC>1: ", ind, "\n")
     
   }
+  
+  row1 <- ggarrange( plotList[[1]], ncol = 1)
+  row2 <- ggarrange( plotList[[2]],  plotList[[3]], ncol = 2)
+  row3 <- ggarrange( plotList[[4]],  plotList[[5]], ncol = 2)
+  row4 <- ggarrange( plotList[[6]],  plotList[[7]], ncol = 2)
+  
+  ggCut2 <- ggarrange(row1, row2, row3, row4,
+                     ncol = 1,
+                     heights = c(1.2, 1, 1, 1))
+  
+  ggCut3 <- annotate_figure(ggCut2,
+                  top =  mont_j)
+  
+  ggsave(file = 
+           paste0(folderPlot, "/SecCase_", mont_j, ".png"),
+         plot= ggCut3, units="in",dpi=300) #units="in", height=4,
 }
 
