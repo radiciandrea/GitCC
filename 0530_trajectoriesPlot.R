324a3e8ae91c41d417a6b01331419f2d85893ad8
radiciandrea <andrea.radici93@libero.it>
2026-01-04 19:21:57 +0100

    massively modified code

--- 0530_trajectoriesPlot.R
+++ 0530_trajectoriesPlot.R
@@ -18,132 +18,37 @@ library(sf)
 library(data.table)
 library(stats)
 
-mod = ""
+mod = "hot"
 
 # folders
 
 folderSim = paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/DRIAS", mod, "_sim_030")
 folderSimNoDiap = paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/DRIAS", mod, "_sim_030f")
+folderOut = paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/DRIAS", mod, "_sim_050")
 folderData = paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/DRIAS", mod, "_elab")
-folderPlot = paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Scenari climatici/DRIAS", mod, "_sim_0340")
+folderPlot = paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Scenari climatici/DRIAS", mod, "_sim_0530")
 
 # scenarios and cities
 
 scenarios <- c("Cn70", "Hs99","Hg70")
 cities <- c("Montpellier", "Nantes", "Rennes", "Lille", "Paris-est", "Lyon", "Grenoble", "Bordeaux", "Toulouse", "Marseille", "Nice", "Strasbourg", "Clermont-Ferrand")
-IDsSubSet <- c(1040, 5243, 6482, 8915, 7542, 3500, 2936, 2472, 929, 642, 1249, 7379, 3564)
+IDs <- c(1040, 5243, 6482, 8915, 7542, 3500, 2936, 2472, 929, 642, 1249, 7379, 3564)
 
+cities <- c(cities, "46190 Sousceyrac-en-Quercy (Comiac)")
+IDs <- c(IDs,2580)
 
-## 1: diapausing and homodynamic Adults----
-# plot diapausing and homodynamic trajectories for Montpellier
+cityDF = data.frame(cities = cities,
+                    IDs = IDs)
 
+cityDF <- cityDF %>%
+  arrange(IDs)
 
-# high + rcp 8.5 2066-2085
-nameSc = "Hg70"
-IDx = 3500 #Lyon
-IDx = 1040 #Montpellier
+IDsSubSet = cityDF$IDs
+cities = cityDF$cities
 
-colPalette <- c("#D04B45", "#5200A3")
-
-for(IDx in IDsSubSet){
-  
-  cityx = cities[which(IDsSubSet == IDx)]
-  
-  trajAdDF = data.frame(day = rep(1:365, 2*length(scenarios)),
-                        type =rep(c("D", "H"), each = 365*length(scenarios)),
-                        nameSc = rep(rep(scenarios, each = 365), times = 2),
-                        meanDensity = NA,
-                        medianDensity = NA,
-                        IQ = NA,
-                        IIIQ = NA)
-  
-  for(nameSc in scenarios){
-    
-    
-    fileDAdults= list.files(paste0(folderSim,"/"), pattern = paste0("030a_Adults_Drias_", nameSc))
-    fileHAdults= list.files(paste0(folderSimNoDiap,"/"), pattern = paste0("030f_Adults_Drias_", nameSc))
-    
-    DAdultsM = matrix(NA, nrow = 365, ncol = length(fileDAdults))
-    HAdultsM = matrix(NA, nrow = 365, ncol = length(fileDAdults))
-    
-    for(i in 1:length(fileDAdults)){
-      DAdultsM[,i] = readRDS(paste0(folderSim, "/", fileDAdults[i]))[1:365, IDx]
-    }
-    
-    for(i in 1:length(fileHAdults)){
-      HAdultsM[,i] = readRDS(paste0(folderSimNoDiap, "/", fileHAdults[i]))[1:365, IDx]
-    }
-    
-    #smoothing D
-    DAdultsMAM <- stats::filter(DAdultsM, rep(1 / 3, 3), sides = 2)
-    DAdultsMAM[is.na(DAdultsMAM)]=0
-    
-    trajIQ = sapply(1:365, function(t){quantile(DAdultsMAM[t,], 0.25, na.rm = T)})
-    trajMean = rowMeans(DAdultsMAM, na.rm = T)
-    trajMedian = sapply(1:365, function(t){quantile(DAdultsMAM[t,], 0.5, na.rm = T)})
-    trajIIIQ = sapply(1:365, function(t){quantile(DAdultsMAM[t,], 0.75, na.rm = T)})
-    
-    trajAdDF$meanDensity[which(trajAdDF$type == "D" & trajAdDF$nameSc == nameSc)]= trajMean
-    trajAdDF$medianDensity[which(trajAdDF$type == "D" & trajAdDF$nameSc == nameSc)]= trajMedian
-    trajAdDF$IQ[which(trajAdDF$type == "D" & trajAdDF$nameSc == nameSc)]= trajIQ
-    trajAdDF$IIIQ[which(trajAdDF$type == "D" & trajAdDF$nameSc == nameSc)]= trajIIIQ
-    
-    #smoothing H
-    HAdultsMAM <- stats::filter(HAdultsM, rep(1 / 3, 3), sides = 2)
-    HAdultsMAM[is.na(HAdultsMAM)]=0
-    
-    trajIQ = sapply(1:365, function(t){quantile(HAdultsMAM[t,], 0.25, na.rm = T)})
-    trajMean = rowMeans(HAdultsMAM, na.rm = T)
-    trajMedian = sapply(1:365, function(t){quantile(HAdultsMAM[t,], 0.5, na.rm = T)})
-    trajIIIQ = sapply(1:365, function(t){quantile(HAdultsMAM[t,], 0.75, na.rm = T)})
-    
-    # if(nameSc == "Hg70"){
-      trajAdDF$meanDensity[which(trajAdDF$type == "H" & trajAdDF$nameSc == nameSc)]= trajMean
-      trajAdDF$medianDensity[which(trajAdDF$type == "H" & trajAdDF$nameSc == nameSc)]= trajMedian
-      trajAdDF$IQ[which(trajAdDF$type == "H" & trajAdDF$nameSc == nameSc)]= trajIQ
-      trajAdDF$IIIQ[which(trajAdDF$type == "H" & trajAdDF$nameSc == nameSc)]= trajIIIQ
-    # }
-  }
-  
-  trajAdDF <- trajAdDF %>%
-    filter(!is.na(medianDensity))
-  
-  # plot
-  
-  plotCut <-ggplot(data = trajAdDF)+
-    geom_line(aes(x = day, y = log(medianDensity+1), color = type, linetype = nameSc), linewidth = 0.8)+
-    geom_ribbon(aes(x = day, ymin=log(IQ+1), ymax=log(IIIQ+1), fill = type, linetype = nameSc), alpha = 0.2)+
-    scale_fill_discrete(palette = colPalette)+
-    scale_color_discrete(palette = colPalette)+
-    geom_hline(aes(yintercept = log(2)),linetype = 2)+
-    ylim(c(0,9.5))+
-    theme_test()
-  
-  # plotCut <- plotCut +
-  #   theme(legend.position = "none",
-  #         panel.grid = element_blank(), 
-  #         line = element_blank(), 
-  #         rect = element_blank(), 
-  #         text = element_blank(), 
-  #         plot.background = element_rect(fill = "transparent", color = "transparent"))
-  
-  plotCut <- plotCut +
-    theme(legend.position = "none",
-          panel.grid = element_blank(), 
-          axis.title.x = element_blank(),
-          axis.title.y = element_blank(),
-          axis.text.x=element_blank(),
-          axis.text.y=element_blank(),
-          plot.background = element_rect(fill = "transparent", color = "transparent"))
-  
-  ggsave(file = 
-           paste0(folderPlot, "/PoPtraj_", cityx, ".png"),
-         plot= plotCut, units="cm", height=5.5, width = 8.5, dpi=300) #units="in", height=4,
-  
-}
-
-## 2: R0 at different scenarios----
-##  load for another city (Lyon?) also data to plot R0
+nIDs = nrow(cityDF)
+nSs = length(scenarios)
+nD = 365
 
 # epidemic parameters
 #host preference
@@ -156,85 +61,236 @@ IIPdengue = 5 #Benkimoun 2021
 bV2H = 0.5 # b Blagrove 2020
 bH2Vdengue = 0.31 # beta Metelmann 2021
 
-IDx = 3500 #Lyon
-IDx = 1040 #Montpellier
+colPalette1 <- c("#D04B45", "#5200A3")
+colPalette2 <- c( "#21908C", "#FCE724", "#3A528A")
 
-scenarios <- c("Cn70", "Hs99","Hg70")
-cities <- c("Montpellier", "Nantes", "Rennes", "Lille", "Paris-est", "Lyon", "Grenoble", "Bordeaux", "Toulouse", "Marseille", "Nice", "Strasbourg", "Clermont-Ferrand")
-IDsSubSet <- c(1040, 5243, 6482, 8915, 7542, 3500, 2936, 2472, 929, 642, 1249, 7379, 3564)
+## 3: cycle ----
 
-for(IDx in IDsSubSet){
+R0List <- vector(mode = "list", nSs*nIDs)
+HAdList <- vector(mode = "list", nSs*nIDs)
+DAdList <- vector(mode = "list", nSs*nIDs)
+
+tic()
+for(nameSc in scenarios){
   
-  cityx = cities[which(IDsSubSet == IDx)]
+  fileDAdults= list.files(paste0(folderSim,"/"), pattern = paste0("030a_Adults_Drias_", nameSc))
+  fileHAdults= list.files(paste0(folderSimNoDiap,"/"), pattern = paste0("030f_Adults_Drias_", nameSc))
+  filesW= list.files(paste0(folderData,"/"), pattern = paste0("Drias_", nameSc))
   
-  trajR0DF = data.frame(day = rep(1:365, times = length(scenarios)),
-                        nameSc = rep(scenarios, each = 365),
-                        meanR0 = NA,
-                        medianR0 = NA,
-                        IQ = NA,
-                        IIIQ = NA)
+  nRep = length(fileDAdults)
   
-  tic()
-  for(j in 1:length(scenarios)){
+  DAdultsM = array(NA, c(nD, nIDs, nRep))
+  HAdultsM = array(NA, c(nD, nIDs, nRep))
+  tasM = array(NA, c(nD, nIDs, nRep))
+  HM = array(NA, c(nD, nIDs, nRep))
+  R0dengueAdultsM = array(NA, c(nD, nIDs, nRep))
+  
+  for(i in 1:nRep){
+    DAdultsM[, , i] = readRDS(paste0(folderSim, "/", fileDAdults[i]))[1:nD, IDsSubSet]
+    HAdultsM[, , i] = readRDS(paste0(folderSimNoDiap, "/", fileHAdults[i]))[1:nD, IDsSubSet]
     
-    nameSc = scenarios[j]
-    ##
-    filesW= list.files(paste0(folderData,"/"), pattern = paste0("Drias_", nameSc))
-    fileAdults= list.files(paste0(folderSim,"/"), pattern = paste0("030a_Adults_Drias_", nameSc))
-    
-    R0dengueM = matrix(NA, nrow = 365, ncol = length(fileAdults))
-    
-    for(i in 1:length(fileAdults)){
-      Adults = readRDS(paste0(folderSim, "/", fileAdults[i]))[,IDx]
-      tas = readRDS(paste0(folderData, "/", filesW[i])) %>% filter(ID == IDx) %>% pull(tas)
-      H = (readRDS(paste0(folderData, "/", filesW[i])) %>% filter(ID == IDx) %>% pull(pop))[1]
-      
-      #vector to host ratio
-      m = Adults*100/H
-      
-      #demographic parameters
-      muA = -log(0.677 * exp(-0.5*((tas-20.9)/13.2)^6)*tas^0.1) # adult mortality rate
-      muA[which(tas<=0)] = -log(0.677 * exp(-0.5*((tas[which(tas<=0)]-20.9)/13.2)^6))  #correct the problems due to negative values from SI
-      
-      #epidemiological parameters
-      A = (0.0043*tas + 0.0943)/2  #biting rate (Zanardini et al., Caminade 2016, Blagrove 2020)
-      phiA = phiAU*(H>RTh)+phiAR*(H<=RTh)
-      
-      EIPdengue = 1.03*(4*exp(5.15 - 0.123*tas)) #Metelmann 2021
-      
-      # LTS
-      R0dengueM[,i] = ((A*phiA)^2*m/(muA+muA^2*EIPdengue)*bV2H*bH2Vdengue*IIPdengue)[1:365]
-      
-    }
-    
-    #smoothing
-    R0dengueMAM <- stats::filter(R0dengueM, rep(1 / 1, 1), sides = 2)
-    R0dengueMAM[is.na(R0dengueMAM)]=0
-    
-    trajIQ = sapply(1:365, function(t){quantile(R0dengueMAM[t,], 0.25, na.rm = T)})
-    trajMean = rowMeans(R0dengueMAM, na.rm = T)
-    trajMedian = sapply(1:365, function(t){quantile(R0dengueMAM[t,], 0.5, na.rm = T)})
-    trajIIIQ = sapply(1:365, function(t){quantile(R0dengueMAM[t,], 0.75, na.rm = T)})
-    
-    cat(cityx, " - ", nameSc, ": ", sum(trajMean>1), "\n")
-    
-    trajR0DF$meanR0[which(trajR0DF$nameSc == nameSc)]= trajMean
-    trajR0DF$medianR0[which(trajR0DF$nameSc == nameSc)]= trajMedian
-    trajR0DF$IQ[which(trajR0DF$nameSc == nameSc)]= trajIQ
-    trajR0DF$IIIQ[which(trajR0DF$nameSc == nameSc)]= trajIIIQ
-    
-    toc()
+    tasM[, , i] = matrix(readRDS(paste0(folderData, "/", filesW[i]))
+                         %>% filter(ID %in% IDsSubSet)
+                         %>% filter(DOS < (nD+1))
+                         %>% pull(tas)
+                         , nrow = nD)
+    HM[, , i] = matrix(rep(readRDS(paste0(folderData, "/", filesW[i])) 
+                           %>% filter(ID %in% IDsSubSet) 
+                           %>% filter(DOS ==1)
+                           %>% pull(pop)), ncol = nIDs, nrow = nD, byrow = T )
   }
   
-  colPalette <- c( "#21908C", "#FCE724", "#3A528A")
+  #vector to host ratio
+  m = DAdultsM*100/HM
+  
+  #demographic parameters
+  muA = -log(0.677 * exp(-0.5*((tasM-20.9)/13.2)^6)*tasM^0.1) # adult mortality rate
+  muA[which(tasM<=0)] = -log(0.677 * exp(-0.5*((tasM[which(tasM<=0)]-20.9)/13.2)^6))  #correct the problems due to negative values from SI
+  
+  #epidemiological parameters
+  A = (0.0043*tasM + 0.0943)/2  #biting rate (Zanardini et al., Caminade 2016, Blagrove 2020)
+  phiA = phiAU*(HM>RTh)+phiAR*(HM<=RTh)
+  
+  EIPdengue = 1.03*(4+exp(5.15 - 0.123*tasM)) #Metelmann 2021
+  
+  # LTS
+  R0dengueM = ((A*phiA)^2*m/(muA+muA^2*EIPdengue)*bV2H*bH2Vdengue*IIPdengue)
+  
+  #### TRAJ ----
+  
+  #smoothing NO
+  
+  R0dengueMAM <- R0dengueM #stats::filter(R0dengueM, rep(1 / 1, 1), sides = 2)
+  R0dengueMAM[is.na(R0dengueMAM)]=0
+  
+  DAdultsMAM <- DAdultsM #stats::filter(R0dengueM, rep(1 / 1, 1), sides = 2)
+  DAdultsMAM[is.na(DAdultsMAM)]=0
+  
+  HAdultsMAM <- HAdultsM #stats::filter(R0dengueM, rep(1 / 1, 1), sides = 2)
+  HAdultsMAM[is.na(HAdultsMAM)]=0
+  
+  for(j in 1:nIDs){
+    
+    trajR0DF = data.table(city = cities[j],
+                          day = 1:nD,
+                          nameSc = nameSc,
+                          Mean = NA,
+                          Median = NA,
+                          P10=NA,
+                          P90=NA,
+                          IQ = NA,
+                          IIIQ = NA)
+    
+    trajDAdDF = data.table(city = cities[j],
+                           type = "D",
+                           day = 1:nD,
+                           nameSc = nameSc,
+                           Mean = NA,
+                           Median = NA,
+                           P10=NA,
+                           P90=NA,
+                           IQ = NA,
+                           IIIQ = NA)
+    
+    trajHAdDF = data.table(city = cities[j],
+                           type = "H",
+                           day = 1:nD,
+                           nameSc = nameSc,
+                           Mean = NA,
+                           Median = NA,
+                           P10=NA,
+                           P90=NA,
+                           IQ = NA,
+                           IIIQ = NA)
+    
+    trajR0DF$IQ = sapply(1:nD, function(t){quantile(R0dengueMAM[t,j,], 0.25, na.rm = T)})
+    trajR0DF$Mean = sapply(1:nD, function(t){mean(R0dengueMAM[t,j,], na.rm = T)})
+    trajR0DF$Median = sapply(1:nD, function(t){quantile(R0dengueMAM[t,j,], 0.5, na.rm = T)})
+    trajR0DF$IIIQ = sapply(1:nD, function(t){quantile(R0dengueMAM[t,j,], 0.75, na.rm = T)})
+    trajR0DF$P10 = sapply(1:nD, function(t){quantile(R0dengueMAM[t,j,], 0.1, na.rm = T)})
+    trajR0DF$P90 = sapply(1:nD, function(t){quantile(R0dengueMAM[t,j,], 0.9, na.rm = T)})
+    
+    trajDAdDF$IQ = sapply(1:nD, function(t){quantile(DAdultsMAM[t,j,], 0.25, na.rm = T)})
+    trajDAdDF$Mean = sapply(1:nD, function(t){mean(DAdultsMAM[t,j,], na.rm = T)})
+    trajDAdDF$Median = sapply(1:nD, function(t){quantile(DAdultsMAM[t,j,], 0.5, na.rm = T)})
+    trajDAdDF$IIIQ = sapply(1:nD, function(t){quantile(DAdultsMAM[t,j,], 0.75, na.rm = T)})
+    trajDAdDF$P10 = sapply(1:nD, function(t){quantile(DAdultsMAM[t,j,], 0.1, na.rm = T)})
+    trajDAdDF$P90 = sapply(1:nD, function(t){quantile(DAdultsMAM[t,j,], 0.9, na.rm = T)})
+    
+    trajHAdDF$IQ = sapply(1:nD, function(t){quantile(HAdultsMAM[t,j,], 0.25, na.rm = T)})
+    trajHAdDF$Mean = sapply(1:nD, function(t){mean(HAdultsMAM[t,j,], na.rm = T)})
+    trajHAdDF$Median = sapply(1:nD, function(t){quantile(HAdultsMAM[t,j,], 0.5, na.rm = T)})
+    trajHAdDF$IIIQ = sapply(1:nD, function(t){quantile(HAdultsMAM[t,j,], 0.75, na.rm = T)})
+    trajHAdDF$P10 = sapply(1:nD, function(t){quantile(HAdultsMAM[t,j,], 0.1, na.rm = T)})
+    trajHAdDF$P90 = sapply(1:nD, function(t){quantile(HAdultsMAM[t,j,], 0.9, na.rm = T)})
+    
+    
+    cat(cities[j], " - ", nameSc, ": ", sum(trajR0DF$Mean>1), "\n")
+    toc()
+    
+    R0List[[(which(scenarios == nameSc)-1)*nIDs+j]] = trajR0DF
+    DAdList[[(which(scenarios == nameSc)-1)*nIDs+j]] = trajDAdDF
+    HAdList[[(which(scenarios == nameSc)-1)*nIDs+j]] = trajHAdDF
+    
+  }
+}
+
+trajR0DT <- data.table::rbindlist(R0List)
+trajDAdDT <- data.table::rbindlist(DAdList)
+trajHAdDT <- data.table::rbindlist(HAdList)
+
+### save and load
+saveRDS(trajR0DT, file = paste0(folderOut, "/trajR0DT.rds"))
+saveRDS(trajDAdDT, paste0(folderOut, "/trajDAdDT.rds"))
+saveRDS(trajHAdDT, paste0(folderOut, "/trajHAdDT.rds"))
+
+trajR0DT <- readRDS(paste0(folderOut, "/trajR0DT.rds"))
+trajDAdDT <- readRDS(paste0(folderOut, "/trajDAdDT.rds"))
+trajHAdDT <- readRDS(paste0(folderOut, "/trajHAdDT.rds"))
+
+trajAdDT = rbind(trajDAdDT, trajHAdDT)
+                     
+###################################################################################
+
+
+# plot
+
+for(cityx in cities){
+  
+  trajAdDF <- trajAdDT %>%
+    filter(city == cityx)
+  
+  # plotCut <-ggplot(data = trajAdDF)+
+  #   geom_line(aes(x = day, y = log(Median+1), color = type, linetype = nameSc), linewidth = 0.8)+
+  #   geom_ribbon(aes(x = day, ymin=log(IQ+1), ymax=log(IIIQ+1), fill = type, linetype = nameSc), alpha = 0.2)+
+  #   scale_fill_discrete(palette = colPalette1)+
+  #   scale_color_discrete(palette = colPalette1)+
+  #   geom_hline(aes(yintercept = log(2)),linetype = 2)+
+  #   ylim(c(0,9.5))+
+  #   theme_test()
+  
+  maxY = ceil(max(trajAdDF$IIIQ)/10^(floor(log(max(trajAdDF$IIIQ), 10))-1))*10^(floor(log(max(trajAdDF$IIIQ), 10))-1)
+  
+  # plotCut <-ggplot(data = trajAdDF)+
+  #   geom_line(aes(x = day, y = Median, color = type, linetype = nameSc), linewidth = 0.8)+
+  #   geom_ribbon(aes(x = day, ymin=IQ, ymax=IIIQ, fill = type, linetype = nameSc), alpha = 0.2)+
+  #   scale_fill_discrete(palette = colPalette1)+
+  #   scale_color_discrete(palette = colPalette1)+
+  #   scale_y_continuous(breaks = c(0, maxY/3, 2*maxY/3, maxY), labels = c("", round(maxY/3,0), round(2*maxY/3), maxY))+
+  #   geom_hline(aes(yintercept = 2),linetype = 2)+
+  #   theme_test()
+  
+  # plotCut <- plotCut +
+  #   theme(legend.position = "none",
+  #         panel.grid = element_blank(), 
+  #         axis.title.x = element_blank(),
+  #         axis.title.y = element_blank(),
+  #         axis.text.x=element_blank(),
+  #         axis.text.y=element_blank(),
+  #         plot.background = element_rect(fill = "transparent", color = "transparent"))
+
+plotCut <-ggplot(data = trajAdDF)+
+  geom_line(aes(x = day, y = Mean, color = type, linetype = nameSc), linewidth = 0.8)+
+  geom_ribbon(aes(x = day, ymin=P10, ymax=P90, fill = type, linetype = nameSc), alpha = 0.2)+
+  scale_fill_discrete(palette = colPalette1)+
+  scale_color_discrete(palette = colPalette1)+
+  scale_y_continuous(breaks = c(0, maxY/3, 2*maxY/3, maxY), labels = c("", round(maxY/3,0), round(2*maxY/3), maxY))+
+  geom_hline(aes(yintercept = 2),linetype = 2)+
+  theme_test()
+  
+  plotCut <- plotCut +
+    theme(legend.position = "none",
+          panel.grid = element_blank(),
+          axis.text.y = element_text(margin = margin(r = -25, unit = "pt")),
+          axis.title.y = element_blank(),
+          axis.text.x = element_blank(), 
+          axis.title.x = element_blank(),
+          plot.background = element_rect(fill = "transparent", color = "transparent"))
+    
+  ggsave(file = 
+           paste0(folderPlot, "/PoPtraj_", cityx, ".png"),
+         plot= plotCut, units="cm", height=5.5, width = 8.5, dpi=300) #units="in", height=4,
+  
+  
+  trajR0DF <- trajR0DT %>%
+    filter(city == cityx)
+  
+  # plotCut <- ggplot(data = trajR0DF)+
+  #   geom_line(aes(x = day, y = Median, color = nameSc), linewidth = 0.8)+
+  #   geom_ribbon(aes(x = day, ymin=IQ, ymax=IIIQ, fill = nameSc), alpha = 0.2)+
+  #   scale_fill_discrete(palette = colPalette2)+
+  #   scale_color_discrete(palette = colPalette2)+
+  #   geom_hline(aes(yintercept = 1),linetype = 2)+
+  #   ylim(c(0,2))+
+  #   theme_test()
   
   plotCut <- ggplot(data = trajR0DF)+
-    geom_line(aes(x = day, y = medianR0, color = nameSc), linewidth = 0.8)+
-    geom_ribbon(aes(x = day, ymin=IQ, ymax=IIIQ, fill = nameSc), alpha = 0.2)+
-    scale_fill_discrete(palette = colPalette)+
-    scale_color_discrete(palette = colPalette)+
+    geom_line(aes(x = day, y = Mean, color = nameSc), linewidth = 0.8)+
+    geom_ribbon(aes(x = day, ymin=P10, ymax=P90, fill = nameSc), alpha = 0.2)+
+    scale_fill_discrete(palette = colPalette2)+
+    scale_color_discrete(palette = colPalette2)+
     geom_hline(aes(yintercept = 1),linetype = 2)+
-    ylim(c(0,7))+
+    ylim(c(0,max(2,max(trajR0DF$P90, na.rm = T))))+
     theme_test()
   
   # plotCut <- plotCut +
@@ -259,4 +315,3 @@ for(IDx in IDsSubSet){
          plot= plotCut, units="cm", height=5.5, width = 8.5, dpi=300) #units="in", height=4,
   
 }
-
